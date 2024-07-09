1db4fd8d1608d624a6fefd293e19d7552aaa425b
Mateusz Kulesza <ev45ive@gmail.com>
2024-07-09 16:50:52 +0100

    Injecting parents - warning: High Coupling

--- src/app/playlists/components/playlist-editor/playlist-editor.component.ts
+++ src/app/playlists/components/playlist-editor/playlist-editor.component.ts
@@ -1,19 +1,37 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+} from '@angular/core';
+import { PlaylistsComponent } from '../../playlists.component';
+import { AppComponent } from '../../../app.component';
 
 @Component({
   selector: 'app-playlist-editor',
   templateUrl: './playlist-editor.component.html',
   styleUrl: './playlist-editor.component.scss',
-  // changeDetection: ChangeDetectionStrategy.OnPush, // d|-_-|b  
+  // changeDetection: ChangeDetectionStrategy.OnPush, // d|-_-|b
 })
 export class PlaylistEditorComponent {
-  
   playlist = {
     id: '123',
     name: 'Playlist 123',
     public: true,
     description: 'Cool playlsit',
   };
+
+  // constructor(private elem: ElementRef) {}
+
+  // Injecting parents - warning: High Coupling
+  constructor(private parent: PlaylistsComponent, private app: AppComponent) {
+    console.log(parent, app);
+    debugger;
+  }
+
+  onCancel(){
+    this.parent.mode = 'details'
+  }
 
   // constructor(private cdr: ChangeDetectorRef){}
 
