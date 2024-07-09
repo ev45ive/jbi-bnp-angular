fd7b56298ac4ede449e66113ab02ceb85befc038
Mateusz Kulesza <ev45ive@gmail.com>
2024-07-09 12:55:26 +0100

    Exercise: Interpolate data with {{ }}

--- src/app/playlists/components/playlist-details/playlist-details.component.ts
+++ src/app/playlists/components/playlist-details/playlist-details.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-playlist-details',
   templateUrl: './playlist-details.component.html',
-  styleUrl: './playlist-details.component.scss'
+  styleUrl: './playlist-details.component.scss',
 })
 export class PlaylistDetailsComponent {
-
+  
+  playlist = {
+    id: '123',
+    name: 'Playlist 123',
+    public: true,
+    description: 'Cool playlsit',
+  };
 }
