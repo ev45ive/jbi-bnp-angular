0493714586ed16a4f4b141d8801cd02f00d1c0e4
Mateusz Kulesza <ev45ive@gmail.com>
2024-07-09 11:41:29 +0100

    Playlists routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+  import { NgModule } from '@angular/core';
+  import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+  const routes: Routes = [{ path: 'playlists', loadChildren: () => import('./playlists/playlists.module').then(m => m.PlaylistsModule) }];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+  @NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
