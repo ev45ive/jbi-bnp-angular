918d643573250172723791c0a07b388acfaaf771
Mateusz Kulesza <ev45ive@gmail.com>
2024-07-10 10:20:00 +0100

    Exercise: Select playlist on click to activate it

--- src/app/playlists/components/playlist-list/playlist-list.component.ts
+++ src/app/playlists/components/playlist-list/playlist-list.component.ts
@@ -1,32 +1,36 @@
 import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
-NgFor
+NgFor;
+const mockPlaylists = [
+  {
+    id: '123',
+    name: 'Playlist 123',
+    public: true,
+    description: 'Cool playlsit',
+  },
+  {
+    id: '234',
+    name: 'Playlist 234',
+    public: false,
+    description: 'Banana playlsit',
+  },
+  {
+    id: '345',
+    name: 'Playlist 345',
+    public: true,
+    description: 'Best playlsit',
+  },
+];
 @Component({
   selector: 'app-playlist-list',
   templateUrl: './playlist-list.component.html',
   styleUrl: './playlist-list.component.scss',
 })
 export class PlaylistListComponent {
-  
-  playlists = [
-    {
-      id: '123',
-      name: 'Playlist 123',
-      public: true,
-      description: 'Cool playlsit',
-    },
-    {
-      id: '234',
-      name: 'Playlist 234',
-      public: false,
-      description: 'Banana playlsit',
-    },
-    {
-      id: '345',
-      name: 'Playlist 345',
-      public: true,
-      description: 'Best playlsit',
-    },
-  ];
+  playlists = mockPlaylists;
+  selectedId = '234';
+  select(id: string) {
+    this.selectedId = id;
+  }
 }
