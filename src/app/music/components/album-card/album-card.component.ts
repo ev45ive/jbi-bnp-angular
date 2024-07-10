17c8b74a6615a2a2b2f7fdc281cb741e8f6caa01
Mateusz Kulesza <ev45ive@gmail.com>
2024-07-10 16:17:47 +0100

    Signal input - Variable "in the box" - call signal() to watch it

--- src/app/music/components/album-card/album-card.component.ts
+++ src/app/music/components/album-card/album-card.component.ts
@@ -1,15 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { SHARED_IMPORTS } from '../../../shared/shared.module';
+import { Album } from '../../../core/model/Album';
 
 @Component({
   selector: 'app-album-card',
   standalone: true,
-  imports: [
-    SHARED_IMPORTS
-  ],
+  imports: [SHARED_IMPORTS],
   templateUrl: './album-card.component.html',
-  styleUrl: './album-card.component.scss'
+  styleUrl: './album-card.component.scss',
 })
 export class AlbumCardComponent {
-
+  album = input.required<Album>();
 }
