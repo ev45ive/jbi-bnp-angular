61a49db56c798352821161c2d16fdc186b3472f2
Mateusz Kulesza <ev45ive@gmail.com>
2024-07-10 16:07:07 +0100

    inject() vs constructor @Inject()

--- src/app/music/containers/album-search-view/album-search-view.component.ts
+++ src/app/music/containers/album-search-view/album-search-view.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
-import { AlbumCardComponent } from "../../components/album-card/album-card.component";
-import { SearchFormComponent } from "../../components/search-form/search-form.component";
+import { Component, Inject, inject } from '@angular/core';
+import { AlbumCardComponent } from '../../components/album-card/album-card.component';
+import { SearchFormComponent } from '../../components/search-form/search-form.component';
+import { MusicApiService } from '../../../core/services/music-api.service';
 
 @Component({
   selector: 'app-album-search-view',
   standalone: true,
   imports: [AlbumCardComponent, SearchFormComponent],
   templateUrl: './album-search-view.component.html',
-  styleUrl: './album-search-view.component.scss'
+  styleUrl: './album-search-view.component.scss',
 })
 export class AlbumSearchViewComponent {
 
+  api = inject(MusicApiService)
+
+  // constructor(
+  //   // @Optional() @Inject(MusicApiToken) private api: IMusicApi
+  //   private api: MusicApiService,
+  // ) {}
 }
