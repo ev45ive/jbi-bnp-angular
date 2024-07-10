5286cf5594200528b0c6410df348433d1864aeef
Mateusz Kulesza <ev45ive@gmail.com>
2024-07-10 09:56:45 +0100

    Material design imports + Shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,21 +1,41 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
-// ng g shared -m playlists 
+// ng g shared -m playlists
 
 const SHARED_IMPORTS = [
   CommonModule,
   FormsModule,
-]
+  MatButtonModule,
+  MatListModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatDividerModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatIconModule,
+  MatSelectModule,
+  MatMenuModule,
+  MatSidenavModule,
+  MatToolbarModule,
+];
 
 @NgModule({
   declarations: [],
-  imports: [
-    SHARED_IMPORTS, 
-  ],
-  exports:[
-    SHARED_IMPORTS, 
-  ]
+  imports: [SHARED_IMPORTS],
+  exports: [SHARED_IMPORTS],
 })
-export class SharedModule { }
+export class SharedModule {}
