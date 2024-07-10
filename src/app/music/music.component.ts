de30b6a421217ac479cb84a26e6f8c2add9cb085
Mateusz Kulesza <ev45ive@gmail.com>
2024-07-10 15:12:50 +0100

    Standalone component is a 'module'

--- src/app/music/music.component.ts
+++ src/app/music/music.component.ts
@@ -2,21 +2,22 @@ import { Component } from '@angular/core';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-music',
   templateUrl: './music.component.html',
-  styleUrl: './music.component.scss'
+  styleUrl: './music.component.scss',
+  standalone: true, // 15+ -> SCAM,
+  imports: [RouterOutlet],
+  providers: [],
 })
-export class MusicComponent {
-
-}
+export class MusicComponent {}
 
 // SCAM - Single Component Angular Module
-
-@NgModule({
-  declarations: [ MusicComponent ],
-  imports: [ SharedModule ],
-  exports: [ MusicComponent ],
-})
-export class MusicComponentModule {}
+// @NgModule({
+//   declarations: [ MusicComponent ],
+//   imports: [ SharedModule, RouterModule ],
+//   exports: [ MusicComponent ],
+// })
+// export class MusicComponentModule {}
--- src/app/music/music.module.ts
+++ src/app/music/music.module.ts
@@ -2,15 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MusicRoutingModule } from './music-routing.module';
-import { MusicComponent, MusicComponentModule } from './music.component';
+import { MusicComponent } from './music.component';
 import { SharedModule } from '../shared/shared.module';
-
 
 @NgModule({
   imports: [
-    MusicComponentModule,
-    SharedModule,
     MusicRoutingModule
-  ]
+  ],
 })
-export class MusicModule { }
+export class MusicModule {}
