4f1bceefc7645125d49030ef9fcca67aa4ff5218
Mateusz Kulesza <ev45ive@gmail.com>
2024-07-10 16:24:53 +0100

    ng g environments

--- src/app/core/services/music-api.service.ts
+++ src/app/core/services/music-api.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@angular/core';
 import { mockAlbums } from '../model/mockAlbums';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root' // Singleton lazy
 })
 export class MusicApiService {
+
+  // api_url = 'https://api.spotify.com/v1/search'
+  api_url = environment.apiConfig.url
   
   constructor() { }
 
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  apiConfig: { url: 'https://api.spotify.com/v1/' },
+  //   envSUPER_SECRET : process.env['SUPER_SECRET']// No!
+};
