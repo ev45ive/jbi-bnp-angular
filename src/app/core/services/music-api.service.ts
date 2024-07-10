87f383a531554b936eba3fa83dd1a8234f082973
Mateusz Kulesza <ev45ive@gmail.com>
2024-07-10 16:40:39 +0100

    HttpClient, withFetch + request

--- src/app/core/services/music-api.service.ts
+++ src/app/core/services/music-api.service.ts
@@ -2,21 +2,30 @@ import { Injectable, inject } from '@angular/core';
 import { mockAlbums } from '../model/mockAlbums';
 import { environment } from '../../../environments/environment';
 import { API_URL } from '../../tokens';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root', // Singleton lazy
 })
 export class MusicApiService {
-  // api_url = 'https://api.spotify.com/v1/search'
-  // api_url = environment.apiConfig.url
-
   api_url = inject(API_URL).url;
+  http = inject(HttpClient);
 
   constructor() {}
 
   searchAlbums(query = 'batman') {
-    console.log('Search',this.api_url,query);
-    
+    console.log('Search', this.api_url, query);
+
+    this.http.get(this.api_url + 'search', {
+      headers: {
+        Authorization: `Bearer ilikepancakesmuch`,
+      },
+      params: {
+        type: 'album',
+        q: query,
+      },
+    });
+
     return mockAlbums;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { AppComponent } from './app/app.component';
 import {
   bootstrapApplication,
   provideClientHydration,
+  withHttpTransferCacheOptions,
 } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
 import routes from './app/app-routing.module';
 import { API_URL } from './app/tokens';
 import { environment } from './environments/environment';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 // platformBrowserDynamic().bootstrapModule(AppModule, {
 //   ngZoneEventCoalescing: true
@@ -19,19 +21,16 @@ bootstrapApplication(AppComponent, {
   providers: [
     provideClientHydration(),
     provideAnimationsAsync(),
+    provideHttpClient(
+      withFetch(),
+      // withHttpTransferCacheOptions({}), //SSR
+    ),
     provideRouter(routes),
     {
       provide: API_URL,
       useValue: {
         url: environment.apiConfig.url,
       },
-    },
-    // Override:
-    // {
-    //   provide: API_URL,
-    //   useValue: {
-    //     url: 'http://localhost/api',
-    //   },
-    // },
+    }
   ],
 });
