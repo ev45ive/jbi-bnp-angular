fc409deff107e707b481ca61008b04bfa5b259f4
Mateusz Kulesza <ev45ive@gmail.com>
2024-07-11 10:22:55 +0100

    Fix SSR missing providers errors

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,19 +1,34 @@
 // export { AppServerModule as default } from './app/app.module.server';
 
-import { bootstrapApplication, provideClientHydration } from "@angular/platform-browser";
-import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
-import { provideRouter } from "@angular/router";
-import routes from "./app/app-routing.module";
-import { AppComponent } from "./app/app.component";
-import { provideNoopAnimations } from "@angular/platform-browser/animations";
+import {
+  bootstrapApplication,
+  provideClientHydration,
+} from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideRouter } from '@angular/router';
+import routes from './app/app-routing.module';
+import { AppComponent } from './app/app.component';
+import { provideNoopAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { API_URL } from './app/tokens';
+import { environment } from './environments/environment';
 
+export default () =>
+  bootstrapApplication(AppComponent, {
+    providers: [
+      provideClientHydration(),
+      provideNoopAnimations(),
+      provideRouter(routes),
+      provideHttpClient(
+        withFetch(),
+        // withHttpTransferCacheOptions({}), //SSR
+      ),
 
-export default () => 
-    bootstrapApplication(AppComponent,{
-      providers:[
-        provideClientHydration(),
-        provideNoopAnimations(),
-        provideRouter(routes),
-      ]
-    });
-    
+      {
+        provide: API_URL,
+        useValue: {
+          url: environment.apiConfig.url,
+        },
+      },
+    ],
+  });
