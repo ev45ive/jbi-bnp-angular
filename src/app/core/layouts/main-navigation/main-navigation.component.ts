e1a283c09b75bd36a1d460ab3426c8478170250e
Mateusz Kulesza <ev45ive@gmail.com>
2024-07-11 10:56:07 +0100

    remove debugger

--- src/app/core/layouts/main-navigation/main-navigation.component.ts
+++ src/app/core/layouts/main-navigation/main-navigation.component.ts
@@ -1,10 +1,10 @@
-import { Component, inject } from '@angular/core';
+import { Component, ContentChild, ContentChildren, QueryList, inject } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { AsyncPipe } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
+import { MatListModule, MatNavList } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
@@ -21,14 +21,25 @@ import { map, shareReplay } from 'rxjs/operators';
     MatListModule,
     MatIconModule,
     AsyncPipe,
-  ]
+  ],
 })
 export class MainNavigationComponent {
   private breakpointObserver = inject(BreakpointObserver);
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
     .pipe(
-      map(result => result.matches),
-      shareReplay()
+      map((result) => result.matches),
+      shareReplay(),
     );
+
+  @ContentChildren(MatNavList)
+  lists?: QueryList<MatNavList>
+
+  @ContentChild(MatNavList)
+  list: any;
+
+  ngAfterContentInit(): void {
+    this.list;
+  }
 }
