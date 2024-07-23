02345612bec823f5877721498fb2c188c4215b34
Mateusz Kulesza <ev45ive@gmail.com>
2024-07-23 16:29:50 +0100

    add missing @types/**   -  assert ENV with if (!process.env[xx]) throw

--- trainer/2_ts_node_project/src/index.ts
+++ trainer/2_ts_node_project/src/index.ts
@@ -1,9 +1,29 @@
-import fs from 'node:fs'
+import express from 'express';
+import expressSession from 'express-session';
 
-console.log('Hello NodeJS', process.argv)
+const app = express();
 
+const secret = process.env['SECRT'];
 
-// sourceMap + node --enable-source-maps 
+if (!secret) throw new Error('Missing ENV Secret'); /* else { secret } */
+
+app.use(
+  expressSession({
+    secret,
+  }),
+);
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello Node Express</h1>');
+});
+
+console.log('Hello NodeJS', process.argv);
+
+app.listen(8080, '0.0.0.0', () => {
+  console.log(`Listening on http://0.0.0.0:8080/`);
+});
+
+// sourceMap + node --enable-source-maps
 // throw new Error('Ups..') //  2_ts_node_project\src\index.ts:5:7)
 
 // ;[].toReversed() // lib esnext
