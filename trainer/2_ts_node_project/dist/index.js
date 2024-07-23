cbdcebec3dcfea2c535b9d22efce068e789b3418
Mateusz Kulesza <ev45ive@gmail.com>
2024-07-23 16:14:50 +0100

    remove and ignore - node-modules, dist

--- trainer/2_ts_node_project/dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-console.log('Hello NodeJS', process.argv);
-throw new Error('Ups..');
-// ;[].toReversed() // lib esnext
-// Lib: []
-// window.document.querySelector('div') // Error
-//# sourceMappingURL=index.js.map
