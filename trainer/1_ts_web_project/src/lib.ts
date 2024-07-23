93d7dcd827086d290cbf45b158b297da73642fab
Mateusz Kulesza <ev45ive@gmail.com>
2024-07-23 14:37:01 +0100

    Type casting (TS assuming) VS type narrowing (JS checking!)

--- trainer/1_ts_web_project/src/lib.ts
+++ trainer/1_ts_web_project/src/lib.ts
@@ -1,3 +1,16 @@
 export const updateTitle = (text: string) => {
-  document.querySelector('h1');
+  // HTMLHeadingElement | null
+  const h1Elem = document.querySelector('h1');
+
+  (h1Elem as any).innerHTML = 'Anyying?';
+  (h1Elem as HTMLHeadingElement).innerHTML = 'Anyying?';
+  h1Elem!.innerHTML = 'Anyying?'; // Lets Assume THERE IS NO NULL! ;-)
+
+  // --- VS ---
+
+  // 'h1' is possibly 'null'
+  if (h1Elem) {
+    h1Elem.innerHTML = 'Banana';
+  }
+  //   h1Elem?.innerHTML = 'Anyying?'; // cannot assign to null/undefined
 };
