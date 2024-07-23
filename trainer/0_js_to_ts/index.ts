1da2deeaccf1547bc647139bd8e209f7cd905c19
Mateusz Kulesza <ev45ive@gmail.com>
2024-07-23 11:56:23 +0100

    Overloads | Unions

--- trainer/0_js_to_ts/index.ts
+++ trainer/0_js_to_ts/index.ts
@@ -3,12 +3,19 @@
 // function add(a, b) {
 // function add(a: any, b: any) {// non strict
 
-function add(a: string | number | null, b?: string | number ) {
+/**
+ * Overloads
+ */
+function add(a: string, b: string): string;
+function add(a: number, b: number): number;
+function add(a: number, b: number, c: number): number;
+
+function add(a: any, b: any, c?: number) {
   return a + b;
 }
 
-add(null, undefined); // non Strict
-add(null); // non Strict
+// add(null, undefined); // non Strict
+// add(null); // non Strict
 
 add(1, 2);
 add('Alice', 'has a cat');
