58f595b4ae16fa9e5b19ebdfd693a0e90289039f
Mateusz Kulesza <ev45ive@gmail.com>
2024-07-24 16:32:13 +0100

    Narrowing UUID

--- src/examples/narrowing.ts
+++ src/examples/narrowing.ts
@@ -1,7 +1,18 @@
-function normalizeUUID(uuid: number | string) {
-  // lowercase
-  // Integer
+type AllowedUUIDTypes = number | string  // | { uuid: 123 };
+
+// Function lacks ending return statement and return type does not include 'undefined'
+
+function normalizeUUID(uuid: AllowedUUIDTypes): string {
+  if (typeof uuid === 'number') {
+    return uuid.toFixed(0); //  string
+  }
+
+  if (typeof uuid === 'string') {
+    return uuid.toLowerCase();
+  }
+  uuid satisfies never;
+  throw new Error('Not valid uuid' + uuid);
 }
 
-const result1: string = normalizeUUID(123.00);  /// '123'
+const result1: string = normalizeUUID(123.0); /// '123'
 const result2: string = normalizeUUID('BaNaNaNa Batman!'); // 'bananana batman!'
