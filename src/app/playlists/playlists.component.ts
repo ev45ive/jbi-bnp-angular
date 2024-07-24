273ff457e4da7b422c655220ebc750eef55d77dd
Mateusz Kulesza <ev45ive@gmail.com>
2024-07-24 14:51:13 +0100

    Properties, initialisers, optional, widening, narrowing - literal, as const, literal union (enum)

--- src/app/playlists/playlists.component.ts
+++ src/app/playlists/playlists.component.ts
@@ -10,6 +10,10 @@ import {
   MatFormFieldDefaultOptions,
 } from '@angular/material/form-field';
 
+type DisplayModes = 'details' | 'editor' | 'creator'  
+// type DisplayModes = 'details' | 'editor' | 'creator' | string 
+// type DisplayModes = 'details' | 'editor' | 'creator' | (string & {})
+
 @Component({
   selector: 'app-playlists',
   templateUrl: './playlists.component.html',
@@ -26,16 +30,34 @@ import {
   ],
 })
 export class PlaylistsComponent {
+  selectedId = '';
+  // selectedId: string = '';
+  // selectedId?: string; // string | undefined
+  // selectedId? = '';
+
+  // readonly SOME_CONSTANT = 'BANANA'; // "BANANA" literal type
+  // if(this.SOME_CONSTANT == 'APPLE'){}
+
+  // mode: string = 'details';
+  // mode: 'details' = 'details';
+  // mode = 'details' as const;
+  // mode: 'details' | 'editor' | 'creator' = 'creator';
+
+  mode: DisplayModes = 'creator';
+
   playlistsData = mockPlaylists;
-  selectedId = '234';
   selected = mockPlaylists[1];
-  
+
   selectPlaylistById(id: string) {
+    const fromServer = 'de' + 'tails';
+    // if(this.mode === 'details') return
+
+    // if (fromServer == 'details' || fromServer === 'editor')
+    //   this.mode = fromServer;
+
     this.selectedId = id;
     this.selected = this.playlistsData.find((p) => p.id == id)!;
   }
-
-  mode: string = 'details';
 
   showDetails() {
     this.mode = 'details';
