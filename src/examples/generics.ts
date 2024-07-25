80156b71ea710d41329969e38ee102af557e0284
Mateusz Kulesza <ev45ive@gmail.com>
2024-07-25 15:15:08 +0100

    Convieniece generics // Useless generics

--- src/examples/generics.ts
+++ src/examples/generics.ts
@@ -3,7 +3,26 @@ const arr2 = ['alice', 'cat']; // Array<string>
 
 type Box = { value: any };
 
-function logAndReturn(value: any) {
+const b: Box = { value: 'banana' };
+b.value; // any
+
+function logAndReturn(value: string | number | number[]) {
   console.log(value);
   return value;
 }
+const value1 = logAndReturn(arr1);
+const value = logAndReturn('awd');
+value; // any
+
+// Convieniece generics // Useless generics
+declare function parseJSON(json: string): {};
+declare function parseJSON2<T>(json: string): T;
+
+let res: { x: 1 };
+res = parseJSON('{x:1}') as { x: 1 };
+
+res = parseJSON2<{ x: 1 }>('{x:1}');
+
+res.x;
+
+// Useful gernerics
