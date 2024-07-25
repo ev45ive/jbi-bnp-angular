fdcb9b3e259a6beda3fda7e46d80eeb1911cb01c
Mateusz Kulesza <ev45ive@gmail.com>
2024-07-25 10:54:49 +0100

    Dynamic Tabs

--- src/app/experiments/tabs-view/tabs-view.component.ts
+++ src/app/experiments/tabs-view/tabs-view.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
-import { TabsGroupComponent } from "../tabs/tabs-group/tabs-group.component";
-import { TabsGroupItemComponent } from "../tabs/tabs-group-item/tabs-group-item.component";
+import { TabsGroupComponent } from '../tabs/tabs-group/tabs-group.component';
+import { TabsGroupItemComponent } from '../tabs/tabs-group-item/tabs-group-item.component';
+import { SHARED_IMPORTS } from '../../shared/shared.module';
 
 @Component({
   selector: 'app-tabs-view',
   standalone: true,
-  imports: [TabsGroupComponent, TabsGroupItemComponent],
+  imports: [TabsGroupComponent, TabsGroupItemComponent, SHARED_IMPORTS],
   templateUrl: './tabs-view.component.html',
-  styleUrl: './tabs-view.component.scss'
+  styleUrl: './tabs-view.component.scss',
 })
 export class TabsViewComponent {
+  dynamicTabs: number[] = [];
 
+  addTab() {
+    this.dynamicTabs[this.dynamicTabs.length ] = this.dynamicTabs.length ;
+  }
 }
