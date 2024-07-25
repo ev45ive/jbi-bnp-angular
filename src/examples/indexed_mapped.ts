10add1df1be2979be11fd57d102567927827217e
Mateusz Kulesza <ev45ive@gmail.com>
2024-07-25 17:08:03 +0100

    Type Mapping   T => T2

--- src/examples/indexed_mapped.ts
+++ src/examples/indexed_mapped.ts
@@ -1,11 +1,43 @@
 type INdexedType = {
-  // 345: string;
-  //   [k in string]: string;
   [k in number]: string;
 };
 
 const someDIct: INdexedType = {};
 someDIct[123] = 'banana';
 someDIct['345'] = 'banana23';
-// someDIct['etest'] = 'banana'; //
 
+// Restricted INdex
+type RequiredKeys = 'id' | 'name';
+
+type INdexedTypeRequiredKeys = {
+  [k in RequiredKeys]: string;
+};
+const allowedObj: INdexedTypeRequiredKeys = {
+  id: '123',
+  name: '123',
+  //   x: '123',
+};
+
+// Restricted INdex
+type PartialKeys = 'id' | 'name';
+
+type INdexedTypePartialKeys = {
+  // thisIsNormalKeyX ?: string;
+  [k in PartialKeys]?: string;
+};
+const partialObj: INdexedTypePartialKeys = {
+  id: '123',
+  //   name:'adwa' // optional now
+};
+
+// Mapped Types
+// interface Person { id: number, name:string, friends:Person[]  }
+type PersonKeys = keyof Person; // id | name | friends
+
+type PartialPerson = {
+  //   [prop in PersonKeys]?: 'banana';
+  //   [K in PersonKeys]?: Person[K] | 'banana'
+  [K in PersonKeys]?: Person[K];
+};
+// const bananaPerson: PartialPerson = { name: 'banana' };
+const bananaPerson: PartialPerson = { name: 'Not a banana', age: 123 };
